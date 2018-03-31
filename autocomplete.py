1a90c265048ca2e7bc700f9089db2c16d1f9b386
Jay <jay@morena.io>
2018-03-31 16:45:31 -0700

    fixed algorithm to handle longer databases

--- autocomplete.py
+++ autocomplete.py
@@ -1,71 +1,56 @@
-def increment_last_letter(str):
-    if not str:
-        return ""
-
+def increment(str, add_letter):
+    if not str or not add_letter and len(str) == 1:
+        return ''
     chars = list(str)
-    index = len(chars) - 1
+    if add_letter and len(str) == 1:
+        chars.append('a')
+        return ''.join(chars)
+    if (chars[-1] == 'z'):
+        while len(chars) > 0 and chars[-1] == 'z':
+            del chars[-1]
+        if len(chars) <= 1:
+            return ''
+    chars[-1] = chr(ord(chars[-1]) + 1)
+    return  ''.join(chars)
 
-    last_letter = chars[index]
-    if (last_letter == "z"):
-        return increment_last_letter("".join(chars[:-1]))
+def get_next_term_from_terms(first, second):
+    if not first or not second:
+        return ''
+    length = len(first) if len(first) < len(second) else len(second)
+    for i in range(length):
+        if (first[i] != second[i]):
+            return second[:i + 1]
+    return second
 
-    chars[index] = chr(ord(last_letter) + 1)
-    return  "".join(chars)
-
-def find(query, to_find, length):
-    found = query(to_find)
-
-    if (len(found) == 5):
-        last = found[4]
-        length = length + 1
-        chars = list(last[:length])
-        next_to_find =
+def get_next_term(current_term, terms):
+    if (len(terms) > 1):
+        return get_next_term_from_terms(terms[-2], terms[-1])
+    add_letter = len(terms) == 1 and len(current_term) < 26
+    if (len(terms) == 0):
+        return increment(current_term, add_letter)
     else:
-        next_to_find = increment_last_letter(to_find)
+        return increment(terms[-1], add_letter)
 
-    while(next_to_find):
-        # print next_to_find
-        found += find(query, next_to_find, length)
-        next_to_find = increment_last_letter(next_to_find)
+def find(query, term):
+    found = query(term)
+    next_term = get_next_term(term, found)
+    if not next_term:
+        return []
 
-    return found
+    if len(found) > 0 and found[-1].startswith(next_term):
+        del found[-1]
+    return found + find(query, next_term)
 
 def extract(query):
-    results = []
-    for i in range(ord("a"), ord("z")):
-        results += find(query, chr(i), 1)
-    print "ME: ", results
-    return results
+    usernames = []
+    for i in range(ord('a'), ord('z') + 1):
+        usernames += find(query, chr(i))
+    return usernames
 
 def main():
     database = ["abracadara", "al", "alice", "alicia", "allen", "alter", "altercation", "bob", "element", "ello", "eve", "evening", "event", "eventually", "mallory"]
-    print "OK: ", database
     query = lambda prefix: [d for d in database if d.startswith(prefix)][:5]
     assert extract(query) == database
-
-    # database = ["abracadara", "bob", "mallory"]
-    # print "OK: ", database
-    # query = lambda prefix: [d for d in database if d.startswith(prefix)][:5]
-    # assert extract(query) == database
-    #
-    # database = ["a"]
-    # print "OK: ", database
-    # query = lambda prefix: [d for d in database if d.startswith(prefix)][:5]
-    # assert extract(query) == database
-    #
-    # database = []
-    # print "OK: ", database
-    # query = lambda prefix: [d for d in database if d.startswith(prefix)][:5]
-    # assert extract(query) == database
-    #
-    # database = []
-    # with open("/Users/jay/Programming/OpenAI-Scholar-Application-2018/test_usernames.txt") as f:
-    #     database = f.readlines()
-    #     database = [x.strip() for x in database]
-    # print "OK: ", database
-    # query = lambda prefix: [d for d in database if d.startswith(prefix)][:5]
-    # assert extract(query) == database
-
 
 if __name__ == "__main__":
     main()

ac55cc99117b7be98b1fd12854d12b547bf65a27
Jay <jay@morena.io>
2018-03-31 17:26:03 -0700

    added some docstrings

--- autocomplete.py
+++ autocomplete.py
@@ -1,4 +1,10 @@
 def increment(str, add_letter):
+    """Increment a word to the next alphabetical string and return that word as a string.
+
+    Keyword arguments:
+    str -- word to increment
+    add_letter -- true to add a letter at the end if necessary when incrementing, false otherwise
+    """
     if not str or not add_letter and len(str) == 1:
         return ''
     chars = list(str)
@@ -14,6 +20,7 @@ def increment(str, add_letter):
     return  ''.join(chars)
 
 def get_next_term_from_terms(first, second):
+    """Determine the next search term by comparing two terms and return the term as a string."""
     if not first or not second:
         return ''
     length = len(first) if len(first) < len(second) else len(second)
@@ -23,6 +30,7 @@ def get_next_term_from_terms(first, second):
     return second
 
 def get_next_term(current_term, terms):
+    """Determine the next search term using the current terms and return the term as a string."""
     if (len(terms) > 1):
         return get_next_term_from_terms(terms[-2], terms[-1])
     add_letter = len(terms) == 1 and len(current_term) < 26
@@ -32,6 +40,7 @@ def get_next_term(current_term, terms):
         return increment(terms[-1], add_letter)
 
 def find(query, term):
+    """Find usernames by searching for a term using the provided query and return the usernames as a list."""
     found = query(term)
     next_term = get_next_term(term, found)
     if not next_term:
@@ -41,6 +50,7 @@ def find(query, term):
     return found + find(query, next_term)
 
 def extract(query):
+    """Extract all usernames from a database using the given query and return them as a list."""
     usernames = []
     for i in range(ord('a'), ord('z') + 1):
         usernames += find(query, chr(i))
